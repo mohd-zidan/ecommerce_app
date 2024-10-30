b450dfee5172cedd7d84117299d1316ebe1ecd11
Mohammed Zidan <zidan1568@gmail.com>
2024-10-30 10:51:53 +0530

    fix: removed unused imports

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:ecommerce_app/main.dart';
 import 'package:ecommerce_app/models/products.dart';
 import 'package:ecommerce_app/provider/product.dart';
-import 'package:ecommerce_app/screens/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
